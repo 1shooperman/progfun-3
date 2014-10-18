6f148412391385e263141730b7b8cecc0723c1f0
bshoop117 <bshoop216@yahoo.com>
2014-10-18 13:20:40 -0400

    finished decode.. on towards encode!

---
+++ patmat/src/main/scala/patmat/scratch.sc
@@ -0,0 +1,5 @@
+package patmat
+
+object scratch {
+  Huffman.decodedSecret.mkString                  //> res0: String = huffmanestcool
+}
--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -10,8 +10,28 @@ import patmat.Huffman._
 @RunWith(classOf[JUnitRunner])
 class HuffmanSuite extends FunSuite {
   trait TestTrees {
-    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
-    val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+    val t1 = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
+    val t2 = Fork(Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5), Leaf('d', 4), List('a', 'b', 'd'), 9)
+
+    val L4 = Leaf('E', 1)
+    val R4 = Leaf('F', 1)
+
+    val L5 = Leaf('G', 1)
+    val R5 = Leaf('H', 1)
+
+    val L2 = Fork(L4, R4, "EF".toList, 2)
+    val R2 = Fork(L5, R5, "GH".toList, 2)
+
+    val L3 = Leaf('C', 1)
+    val R3 = Leaf('D', 1)
+
+    val L1 = Leaf('B', 3)
+    val R1 = Fork(L3, R3, "CD".toList, 2)
+
+    val LEFT = Fork(L1, R1, "BCD".toList, 5)
+    val RIGHT = Fork(L2, R2, "EFGH".toList, 4)
+
+    val tree = Fork(Leaf('A', 8), Fork(LEFT, RIGHT, "BCDEFGH".toList, 9), "ABCDEFGH".toList, 17)
   }
 
   test("weight of a larger tree") {
@@ -22,8 +42,13 @@ class HuffmanSuite extends FunSuite {
 
   test("chars of a larger tree") {
     new TestTrees {
-      assert(chars(t2) === List('a','b','d'))
+      assert(chars(t2) === List('a', 'b', 'd'))
     }
+  }
+
+  test("times from comments") {
+    val xs: List[(Char, Int)] = List(('a', 2), ('b', 1))
+    assert(times(List('a', 'b', 'a')) === xs)
   }
 
   test("string2chars(\"hello, world\")") {
@@ -31,12 +56,37 @@ class HuffmanSuite extends FunSuite {
   }
 
   test("makeOrderedLeafList for some frequency table") {
-    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
+    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 3)))
+  }
+
+  test("singleton false") {
+    new TestTrees {
+      assert(singleton(List(t1, t2)) === false)
+    }
+  }
+
+  test("singleton true") {
+    new TestTrees {
+      assert(singleton(List(t1)) === true)
+    }
   }
 
   test("combine of some leaf list") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
-    assert(combine(leaflist) === List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
+    assert(combine(leaflist) === List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)))
+  }
+
+  test("decode from assignment details") {
+    val bits: List[Bit] = List(1, 0, 0, 0, 1, 0, 1, 0)
+    new TestTrees {
+      assert(decode(tree, bits) === "BAC".toList)
+    }
+  }
+  
+  test("encode from assignment details") {
+    new TestTrees {
+      assert(encode(tree)("D".toList) === List(1,0,1,1))
+    }
   }
 
   test("decode and encode a very short text should be identity") {
